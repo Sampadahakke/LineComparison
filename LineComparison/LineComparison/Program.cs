3d1926ee4a7637ad7150d9f15cdfd01acf4b42db
Sampada <sampadahakke102@gmail.com>
2022-02-08 22:01:00 +0530

    Adding Length Calculation Problem

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problems");
+            Console.ReadLine();
+
+            LineComparison line = new LineComparison();
+            line.length_line();
+        }
+    }
+}
